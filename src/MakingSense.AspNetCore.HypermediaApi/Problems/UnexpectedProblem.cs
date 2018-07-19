c7f6ff6ee8d83ac1006ecb9fb2a032314dbcf9fd
Andrés Moschini <andresmoschini@gmail.com>
2018-07-19 13:14:15 -0300

    [OT] Refactor UnexpectedProblem

--- src/MakingSense.AspNetCore.HypermediaApi/Problems/UnexpectedProblem.cs
+++ src/MakingSense.AspNetCore.HypermediaApi/Problems/UnexpectedProblem.cs
@@ -6,15 +6,14 @@ namespace MakingSense.AspNetCore.HypermediaApi.Problems
 {
 	public class UnexpectedProblem : Problem
 	{
-		private Exception _exception;
 		public override string title => "Unexpected error";
 		public override int status => StatusCodes.Status500InternalServerError;
 		public override int errorCode => 0;
-		public override string detail => $"An unexpected error was thrown/r/n Exception Message: {_exception.Message} /r/n Exception Type: {_exception.GetType()}";
+		public override string detail { get; }
 
 		public UnexpectedProblem(Exception exception)
 		{
-			_exception = exception;
+			detail = $"An unexpected error was thrown/r/n Exception Message: {exception.Message} /r/n Exception Type: {exception.GetType()}";
 		}
 	}
 }

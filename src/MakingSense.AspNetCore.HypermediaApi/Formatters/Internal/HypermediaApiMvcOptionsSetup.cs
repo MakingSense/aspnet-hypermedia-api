d73194dcd2c39f191da221dae929f7bd87bb4ef7
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2019-05-24 11:03:25 -0300

    Update HypermediaApiMvcOptionsSetup.cs

--- src/MakingSense.AspNetCore.HypermediaApi/Formatters/Internal/HypermediaApiMvcOptionsSetup.cs
+++ src/MakingSense.AspNetCore.HypermediaApi/Formatters/Internal/HypermediaApiMvcOptionsSetup.cs
@@ -18,46 +18,22 @@ namespace MakingSense.AspNetCore.HypermediaApi.Formatters.Internal
 			ObjectPoolProvider objectPoolProvider)
 			: base((options) => ConfigureMvc(
 				options,
-				jsonOptions.Value,
+				jsonOptions.Value.SerializerSettings,
 				loggerFactory,
 				charPool,
-				objectPoolProvider))
+				objectPoolProvider,
+				jsonOptions.Value))
 		{
 		}
 
-		[Obsolete("This method is obsolete and will be removed in a future version.")]
 		public static void ConfigureMvc(
 			MvcOptions options,
 			JsonSerializerSettings serializerSettings,
 			ILoggerFactory loggerFactory,
 			ArrayPool<char> charPool,
-			ObjectPoolProvider objectPoolProvider)
+			ObjectPoolProvider objectPoolProvider,
+			MvcJsonOptions jsonOptions = null)
 		{
-			serializerSettings.Formatting = Formatting.Indented;
-
-			serializerSettings.Converters.Add(new DateTimeOffsetFormatJsonConverter());
-			serializerSettings.DateParseHandling = DateParseHandling.None;
-
-			options.OutputFormatters.Clear();
-			options.OutputFormatters.Add(new JsonOutputFormatter(serializerSettings, charPool));
-
-			options.InputFormatters.Clear();
-			var jsonInputLogger = loggerFactory.CreateLogger<HypermediaApiJsonInputFormatter>();
-			options.InputFormatters.Add(new HypermediaApiJsonInputFormatter(
-				jsonInputLogger,
-				serializerSettings,
-				charPool,
-				objectPoolProvider));
-		}
-
-		public static void ConfigureMvc(
-			MvcOptions options,
-			MvcJsonOptions jsonOptions,
-			ILoggerFactory loggerFactory,
-			ArrayPool<char> charPool,
-			ObjectPoolProvider objectPoolProvider)
-		{
-			JsonSerializerSettings serializerSettings = jsonOptions.SerializerSettings;
 			serializerSettings.Formatting = Formatting.Indented;
 
 			serializerSettings.Converters.Add(new DateTimeOffsetFormatJsonConverter());
